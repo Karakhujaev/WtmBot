5ce67332b717f0a59edc8214fd4ec4fded33b84c
Musoxon Qaraxujayev <musoxonqaraxujayev@gmail.com>
2023-03-31 21:08:04 +0500

    commmit

--- bot.py
+++ bot.py
@@ -1,61 +1,252 @@
-import pandas 
-from get_data import *
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler
+from ast import Try
+from telegram import Update, message
+from email.mime import audio
+from get_data import*
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, CallbackQueryHandler
 from telegram.ext.filters import Filters
+import requests
+from bs4 import BeautifulSoup
+import os
+import telepot
+
+bot = telepot.Bot('5377685059:AAErJgAD3Kel9_j0MVQNx2zOamgCA6wFWAs')
+
+liked = 0
+info = []
+buttons = []
+like_dislike = []
+song_number = 0
+favourites = []
+history = []
+times = 1
+ad_urls = ''
+caption = ''
 
 
-def start(update: Update, context: CallbackContext) -> None:
+def start(update: Update, context: CallbackContext):
     update.message.reply_text(f'Hi {update.effective_user.first_name}')
     update.message.reply_text(f'You can search music')
 
-def search(update: Update, context: CallbackContext) -> None:
+def search(update: Update, context: CallbackContext):
+    global info, times, history, buttons, like_dislike, favourites
+    times = 1
+    message = ""
+    like = 0
+    dislike = 0
+    history.append([info])
+    info.clear()
+
+    chat_id = update.effective_chat.id
+    
+    buttons = [
+        [
+            InlineKeyboardButton('1', callback_data='btn0'),
+            InlineKeyboardButton('2', callback_data='btn1'),
+            InlineKeyboardButton('3', callback_data='btn2'),
+            InlineKeyboardButton('4', callback_data='btn3'),
+            InlineKeyboardButton('5', callback_data='btn4')
+        ],
+        [
+            InlineKeyboardButton('6', callback_data='btn5'),
+            InlineKeyboardButton('7', callback_data='btn6'),
+            InlineKeyboardButton('8', callback_data='btn7'),
+            InlineKeyboardButton('9', callback_data='btn8'),
+            InlineKeyboardButton('10', callback_data='btn9')
+        ],
+        [ 
+            InlineKeyboardButton('⏮', callback_data='btn10'),
+            InlineKeyboardButton('❌', callback_data='btn11'),
+            InlineKeyboardButton('⏭', callback_data='btn12')
+        ]
+    ]
+
+  
+    
     searched = update.message.text
+    lowered = searched.lower()
+    capitalized = searched.title()
+    uppered = searched.upper()
 
-    # result = session.query(Table).filter(Table.name==searched)
-    result = session.query(Table).filter_by(name=searched).first()
-    import pandas 
-from get_data import *
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler
-from telegram.ext.filters import Filters
+    titles = [searched, lowered, capitalized, uppered]
+
+    for method in titles:
+        if session.query(Table).filter_by(author=method).all(): 
+        
+                result = session.query(Table).filter_by(author=method).all()
+
+                for x in range(len(result)):
+                    link = result[x].link
+                    music_name = result[x].name
+                    music_auth = result[x].author
+                    info.append([music_auth, music_name, link, like])
+
+        if session.query(Table).filter_by(name=method).all(): 
+        
+                result = session.query(Table).filter_by(name=method).all()
+
+                for x in range(len(result)):
+                    link = result[x].link
+                    music_name = result[x].name
+                    music_auth = result[x].author
+                    
+                    info.append([music_auth, music_name, link, like])
+    for i in range(len(info)):
+        if i < 10:
+            singer = info[i][0]
+            music = info[i][1]
+            link = info[i][2]
+            index = i+1
+            message += f"{index}. {singer} - {music}\n\n"
+    context.bot.send_message(chat_id=chat_id,text=message, reply_markup=InlineKeyboardMarkup(buttons))
+   
+
+def button(update: Update, context: CallbackContext):
+    global info, times, buttons, ad_urls, like_dislike, favourites, song_number, liked, caption
+
+    chat_id = update.effective_chat.id
+
+    query = update.callback_query
+
+  
+
+    if query.data == f'btn11':
+        query.message.delete()
+
+    elif query.data == f'btn12':
+        message = ''
+        fir = int(f"{times}0")
+        sec = int(f"{times+1}0")
+        index=0
+        ad_urls = []
+        for i in range(fir, sec):
+            if i < sec:
+                song_number = i
+                singer = info[i][0]
+                music = info[i][1]
+                ad_urls.append([info[i][0], info[i][1], info[i][2], info[i][3]])
+                index+=1
+                message += f"{index}. {singer} - {music}\n\n"
+        context.bot.send_message(chat_id=chat_id,text=message, reply_markup=InlineKeyboardMarkup(buttons))
+        query.message.delete()
+        times+=1
+
+    elif query.data == f'btn10' and times > 1:
+        message = ''
+        times -= 1
+        fir = int(f"{times-1}0")
+        sec = int(f"{times}0")
+        index=0
+        ad_urls = []
+        for i in range(fir, sec):
+            if i < sec:
+                song_number = i
+                singer = info[i][0]
+                music = info[i][1]
+                ad_urls.append([info[i][0], info[i][1], info[i][2]])
+                index+=1
+                message += f"{index}. {singer} - {music}\n\n"
+        context.bot.send_message(chat_id=chat_id,text=message, reply_markup=InlineKeyboardMarkup(buttons))
+        query.message.delete()
+
+    elif times > 1:
+        for x in range(10):  
+            if query.data == f'btn{x}':
+                author = ad_urls[x][0]
+                name = ad_urls[x][1]
+                song_number = x
+                url = ad_urls[x][2]
+                song = requests.get(url)
+                with open(f'my_song.mp3', 'wb') as f:
+                    f.write(song.content)
+                like_dislike = [
+                    [
+                        InlineKeyboardButton('❤️', callback_data='btn13'),
+                        InlineKeyboardButton(f'👍{ad_urls[x][3]}', callback_data='btn14')
+                    ]
+                ]
+                caption = f'{author} - {name}\n\nhttps://t.me/muz_zonebot'
+                context.bot.send_audio(chat_id, open('my_song.mp3','rb'),  reply_markup=InlineKeyboardMarkup(like_dislike), caption=caption)
+
+    elif  times == 1:                
+        for x in range(10):  
+            if query.data == f'btn{x}':
+                author = info[x][0]
+                name = info[x][1]
+                url = info[x][2]
+                song_number = x
+                song = requests.get(url)
+                with open(f'my_song.mp3', 'wb') as f:
+                    f.write(song.content)
+                like_dislike = [
+                    [
+                        InlineKeyboardButton('❤️', callback_data='btn13'),
+                        InlineKeyboardButton(f'👍{info[x][3]}', callback_data='btn14')
+                    ]
+                ]
+                caption = f'{author} - {name}\n\nhttps://t.me/muz_zonebot'
+                context.bot.send_audio(chat_id, open('my_song.mp3','rb'),  reply_markup=InlineKeyboardMarkup(like_dislike), caption=caption)
+
+        if query.data == f'btn14':
+            if liked==0:
+                info[song_number][3] += 1
+                print(info[song_number][3])
+                like_dislike = [
+                    [
+                        InlineKeyboardButton('❤️', callback_data='btn13'),
+                        InlineKeyboardButton(f'👍{info[song_number][3]}', callback_data='btn14')
+                    ]
+                ]
+                liked = 1
+                bot.editMessageReplyMarkup({ like_dislike: like_dislike }, { chat_id: chat_id, update.callback_query.inline_message_id: query.message.message_id })
+                
+            elif liked!=0 and info[song_number][3] > 0:
+                info[song_number][3] -= 1
+
+                like_dislike = [
+                    [
+                        InlineKeyboardButton('❤️', callback_data='btn13'),
+                        InlineKeyboardButton(f'👍{info[song_number][3]}', callback_data='btn14')
+                    ]
+                ]
+
+                bot.editMessageReplyMarkup({ like_dislike: like_dislike }, { chat_id: chat_id, update.callback_query.inline_message_id: query.message.message_id })
 
 
-def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(f'Hi {update.effective_user.first_name}')
-    update.message.reply_text(f'You can search music')
 
-def search(update: Update, context: CallbackContext) -> None:
-        searched = update.message.text
+                print(info[song_number][3])
+                liked = 0
 
-        # result = session.query(Table).filter_by(name=searched).first()
-        # print(result)
-        result = session.query(Table).filter_by(author=searched).all()
-        print(result)
-        for i in range(len(result)):
-            mus = result[i]
-            caption = f'{mus.name}\n{mus.author}'
-            update.message.reply_text(f'{mus.link} \n {caption}')
+        elif query.data == f'btn13':
+            favourites.append(info[song_number])
+            print(favourites)
 
-    # caption = f'{result.name}\n{result.author}'
-    # update.message.reply_text(f'{result.link} \n {caption}')
-    # update.message.reply_text(f'{result.link}')
+def list(update: Update, context: CallbackContext):
+    global favourites, song_number
+    chat_id = update.effective_chat.id
     
+    message = ''
 
-updater = Updater('5141714174:AAGoxJEX4-2ndRln3W00c5SJqhXeftxXD58')
+    for i in range(len(favourites)):
+        if i < 10:
+            singer = favourites[i][0]
+            music = favourites[i][1]
+            link = favourites[i][2]
+            index = i+1
+            message += f"{index}. {singer} - {music}\n\n"
+
+    context.bot.send_message(chat_id=chat_id,text=message, reply_markup=InlineKeyboardMarkup(buttons))
+
+    
+updater = Updater('5377685059:AAErJgAD3Kel9_j0MVQNx2zOamgCA6wFWAs')
 
 updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler(CommandHandler('list', list))
 updater.dispatcher.add_handler(MessageHandler(Filters.text, search))
+updater.dispatcher.add_handler(CallbackQueryHandler(button))
+
+
 
 updater.start_polling()
-updater.idle()
-    # update.message.reply_text(f'{result.link}')
-    
-
-updater = Updater('5141714174:AAGoxJEX4-2ndRln3W00c5SJqhXeftxXD58')
-
-updater.dispatcher.add_handler(CommandHandler('start', start))
-updater.dispatcher.add_handler(MessageHandler(Filters.text, search))
-
-updater.start_polling()
+print('Active')
 updater.idle()
--- get_data.py
+++ get_data.py
@@ -1,33 +1,9 @@
-import requests
-from bs4 import BeautifulSoup
+from re import T
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import *
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import pandas as pd
 
-
-# link = f"https://jobmp3.ru/"
-
-# music_info = []
-# for i in range(1,4289):
-#     print(i)
-#     responce = requests.get(f'{link}music/page/{i}/').text
-#     soup = BeautifulSoup(responce, "html.parser")
-#     block = soup.find('div', class_='sect-content')
-#     all_music = block.find_all('div', class_='track-item fx-row fx-middle js-item')
-
-#     for music in all_music:
-#         try:
-#             music_name = music.get('data-title')
-#             music_link = music.get('data-track')
-#             x = music_name.split(' - ')
-#             author = x[0]
-#             name = x[1]
-
-#             music_info.append([name, author, music_link])
-#         except:
-#             pass
 
 engine = create_engine('sqlite:///data.db', echo=False)
 
@@ -47,13 +23,15 @@ class Table(Base):
     author = Column(String)
     link = Column(String)
 
+    
+
     def __repr__(self):
         return f"{self.author}{self.name}{self.link}"
 
 Base.metadata.create_all(engine)
 
 # for x in range(len(music_info)):
-#     session.add(Table(name = music_info[x][0],author = music_info[x][1],link =music_info[x][2]))
+#     session.add(Table(name = music_info[x][0], author = music_info[x][1], link = music_info[x][2]))
 
 
 session.commit()
